dec5e6b9f8412fc5426293d927db5b646676309c
janschneiderou <jan.schneider@ou.nl>
2015-06-17 14:24:59 +0200

    Stable Version
    Faster, better gesture recognition, logs

---
+++ PresentationTrainer/BodyMirror.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//using AForge.Video;
+
+namespace PresentationTrainer
+{
+    /// <summary>
+    /// Interaction logic for BodyMirror.xaml
+    /// </summary>
+    public partial class BodyMirror : UserControl
+    {
+        MainWindow parent;
+       // FileSink filesink;
+    //    VideoFileWriter writer;
+
+        public BodyMirror()
+        {
+            InitializeComponent();
+        }
+        public void initialize(MainWindow parent)
+        {
+            this.parent = parent;
+
+            // create instance of video writer
+        //    writer = new VideoFileWriter();
+            // create new video file
+      //      writer.Open("test.avi", myImage.ActualWidth, myImage.ActualHeight, 25, VideoCodec.MPEG4);
+            // create a bitmap to save into the video file
+
+            parent.videoHandler.multiFrameSourceReader.MultiSourceFrameArrived += multiFrameSourceReader_MultiSourceFrameArrived;
+            
+        }
+
+        void multiFrameSourceReader_MultiSourceFrameArrived(object sender, Microsoft.Kinect.MultiSourceFrameArrivedEventArgs e)
+        {
+            parent.videoHandler.Reader_MultiSourceFrameArrived( sender,  e);
+            myImage.Source = parent.videoHandler.kinectImage.Source;
+
+
+           
+            
+   
+
+       //     writer.WriteVideoFrame(myImage);
+         
+      //      writer.Close();
+
+        }
+        public void close()
+        {
+
+        }
+    }
+}
---
+++ PresentationTrainer/MainWindow.xaml.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.IO.Ports;
+
+namespace PresentationTrainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///
+    public partial class MainWindow : Window
+    {
+        public FreestyleMode freestyleMode;
+        public VolumeCalibration volumeCalibrationMode;
+        public MainMenu mainMenu;
+        public enum States { menu, freestyle, volumeCalibration, individual};
+        public static States myState;
+
+        public static SerialPort hapticPort;
+        public static bool stopGesture = false;
+
+        private KinectSensor kinectSensor;
+        public InfraredFrameReader frameReader = null;
+        public RulesAnalyzer rulesAnalyzer;
+        public RulesAnalyzerFIFO rulesAnalyzerFIFO;
+
+        public VideoHandler videoHandler;
+        public AudioHandler audioHandler;
+        public BodyFrameHandler bodyFrameHandler;
+        public IndividualSkills individualSkills;
+
+        public static string stringStartFinish = "";
+        public static string stringGestures = "";
+        public static string stringInterruptions = "";
+        public static string stringFreestyleFeedbacks = "";
+        public static string stringSpeakingTime = "";
+        public static string stringPausingTime = "";
+        public static string stringIndividualSkills = "";
+        public static string stringIndividualFeedbacks = "";
+        public static string stringMistakes = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myState = new States();
+            myState = States.menu;
+            loadMode();
+
+            this.kinectSensor = KinectSensor.GetDefault();
+            this.kinectSensor.Open();
+            this.frameReader = this.kinectSensor.InfraredFrameSource.OpenReader();
+
+            rulesAnalyzer = new RulesAnalyzer(this);
+            rulesAnalyzerFIFO = new RulesAnalyzerFIFO(this);
+            videoHandler = new VideoHandler(this.kinectSensor);
+            audioHandler = new AudioHandler(this.kinectSensor);
+            bodyFrameHandler = new BodyFrameHandler(this.kinectSensor);
+
+            initializeHaptic();
+        }
+
+        private void initializeHaptic()
+        {
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    hapticPort = new SerialPort(port, 9600);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        #region controlStuff
+        public void loadMode()
+        {
+            switch (myState)
+            {
+                case States.menu:
+                    loadMainMenu();
+                    break;
+                case States.freestyle:
+                    loadFreestyle();
+                    break;
+                case States.volumeCalibration:
+                    loadVolumeCalibration();
+                    break;
+               
+            }
+        }
+
+        public void loadVolumeCalibration()
+        {
+            volumeCalibrationMode = new VolumeCalibration();
+            volumeCalibrationMode.Height = this.ActualHeight;
+            volumeCalibrationMode.Width = this.ActualWidth;
+            MainCanvas.Children.Add(volumeCalibrationMode);
+            Canvas.SetTop(volumeCalibrationMode, 0);
+            Canvas.SetLeft(volumeCalibrationMode, 0);
+            volumeCalibrationMode.Loaded += volumeCalibrationMode_Loaded;
+        }
+
+        void volumeCalibrationMode_Loaded(object sender, RoutedEventArgs e)
+        {
+            volumeCalibrationMode.parent = this;
+            volumeCalibrationMode.loaded();
+        }    
+
+        public void loadMainMenu()
+        {
+            mainMenu = new MainMenu();
+            MainCanvas.Children.Add(mainMenu);
+            Canvas.SetTop(mainMenu, 0);
+            Canvas.SetLeft(mainMenu, 0);
+            mainMenu.Loaded += mainMenu_Loaded;
+        }
+
+        void mainMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainMenu.FreestyleButton.Click += mainMenu_FreestyleClicked;
+            mainMenu.volumeCalibrationButton.Click += volumeCalibrationButton_Click;
+        }
+
+        public void loadFreestyle()
+        {
+
+            if(freestyleMode==null)
+            {
+                freestyleMode = new FreestyleMode();
+            }
+            
+            freestyleMode.Height = this.ActualHeight;
+            freestyleMode.Width = this.ActualWidth;
+            MainCanvas.Children.Add(freestyleMode);
+            Canvas.SetTop(freestyleMode, 0);
+            Canvas.SetLeft(freestyleMode, 0);
+
+            freestyleMode.Loaded += freeStyle_Loaded;
+        }
+
+        public void loadIndividualSkills( PresentationAction pa)
+        {
+            
+        
+            individualSkills = new IndividualSkills(pa);
+            individualSkills.parent = this;
+            MainCanvas.Children.Add(individualSkills);
+            individualSkills.Height = ActualHeight;
+            individualSkills.Width = ActualWidth;
+            Canvas.SetTop(individualSkills, 0);
+            Canvas.SetLeft(individualSkills, 0);
+            myState = States.individual;
+            individualSkills.Loaded += individualSkills_Loaded;
+            
+        }
+
+        void individualSkills_Loaded(object sender, RoutedEventArgs e)
+        {
+            individualSkills.finish.GoBackButton.Click += GoBackButton_Click;
+        }
+
+        void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            myState = States.freestyle;
+            individualSkills.finish.GoBackButton.Click -= GoBackButton_Click;
+            individualSkills.Visibility = Visibility.Collapsed;
+            MainCanvas.Children.Remove(individualSkills);
+
+            if(freestyleMode!=null)
+            {
+                freestyleMode.OrdinaryReturn_Click(null, null);
+            }
+            else
+            {
+                loadMode();
+            }
+            
+            
+        }
+
+        void freeStyle_Loaded(object sender, RoutedEventArgs e)
+        {
+            freestyleMode.parent = this;
+            freestyleMode.loaded();
+        }
+
+        void volumeCalibrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            closeMainMenu();
+            myState = States.volumeCalibration;
+            loadMode();
+            volumeCalibrationMode.backButton.Click += volumeCalibrationMode_backButton_Click;
+        }
+
+        private void volumeCalibrationMode_backButton_Click(object sender, RoutedEventArgs e)
+        {
+            closeVolumeCalibration();
+            myState = States.menu;
+            loadMode();
+            // we might have to unsubscribe (-=) to the click event of the pressed button
+        }
+
+        private void closeVolumeCalibration()
+        {
+            MainCanvas.Children.Remove(volumeCalibrationMode);
+        }
+
+        private void mainMenu_FreestyleClicked(object sender, RoutedEventArgs e)
+        {
+            closeMainMenu();
+            myState = States.freestyle;
+            setInitialStrings();
+            if (freestyleMode != null)
+            {
+                rulesAnalyzerFIFO.resetAll();
+                freestyleMode.OrdinaryReturn_Click(null, null);
+                freestyleMode.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                loadMode();
+            }
+            
+            freestyleMode.stopButton.Click += freeStyleMode_stopButton_Click;
+            //freestyleMode.reportStopButton.Click += freeStyleMode_reportStopButton_Click;
+            //freestyleMode.logStopButton.Click += freeStyleMode_logStopButton_Click;
+        }
+
+       
+        
+        public void freeStyleMode_reportStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Stop everything, then report.
+            //after these two things, close freeStyleMode
+            closeFreeStyleMode();
+            myState = States.menu;
+            loadMode();
+            // we might have to unsubscribe (-=) to the click event of the pressed button
+        }
+
+        public void freeStyleMode_logStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Stop everything, then log it, then report it.
+            //after these three things, close freeStyleMode
+            closeFreeStyleMode();
+            myState = States.menu;
+            loadMode();
+            // we might have to unsubscribe (-=) to the click event of the pressed button
+        }
+        
+        public void freeStyleMode_stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            closeFreeStyleMode();
+            myState = States.menu;
+            loadMode();
+            // we might have to unsubscribe (-=) to the click event of the pressed button
+        }
+
+        public void closeFreeStyleMode()
+        {
+           freestyleMode.setGhostMovingInvisible();
+           freestyleMode.setOldTextInvisible();
+           freestyleMode.setGhostInvisible();
+           freestyleMode.setFeedbackTextInvisible();
+
+          // MainCanvas.Children.Remove(freestyleMode);
+           freestyleMode.Visibility = Visibility.Collapsed;
+           myState = States.menu;
+            
+            loadMode();
+        }
+
+
+        public void setInitialStrings()
+        {
+            stringStartFinish = "<Start Time>" + DateTime.Now.TimeOfDay.TotalMilliseconds + "</Start Time>" + System.Environment.NewLine;
+            stringGestures = "<Gestures>" + System.Environment.NewLine;
+            stringInterruptions = "<Interruptions>" + System.Environment.NewLine;
+            stringFreestyleFeedbacks = "<Freestyle Feedbacks>" + System.Environment.NewLine;
+            stringSpeakingTime = "<Speaking time> " + System.Environment.NewLine;
+            stringPausingTime = "<Pausing time>" + System.Environment.NewLine;
+            stringIndividualSkills = "<Individual Skills>" + System.Environment.NewLine;
+            stringIndividualFeedbacks = "<Individual Skills Feedback>" + System.Environment.NewLine;
+            stringMistakes = "<Mistakes>" + System.Environment.NewLine;
+        }
+        public void setFinalStrings()
+        {
+            stringStartFinish = stringStartFinish + System.Environment.NewLine + "<Finish Time>" + DateTime.Now.TimeOfDay.TotalMilliseconds + "</FinishTime>" + System.Environment.NewLine;
+            stringGestures = stringGestures + System.Environment.NewLine + "</Gestures>" + System.Environment.NewLine;
+            stringInterruptions = stringInterruptions + System.Environment.NewLine + "</Interruptions>" + System.Environment.NewLine;
+            stringFreestyleFeedbacks = stringFreestyleFeedbacks + System.Environment.NewLine + "</Freestyle Feedbacks>" + System.Environment.NewLine;
+            stringSpeakingTime = stringSpeakingTime + System.Environment.NewLine + "</Speaking time>" + System.Environment.NewLine;
+            stringPausingTime = stringPausingTime + System.Environment.NewLine + "</Pausing time>" + System.Environment.NewLine;
+            stringIndividualSkills = stringIndividualSkills + System.Environment.NewLine + "</Individual Skills>" + System.Environment.NewLine;
+            stringIndividualFeedbacks = stringIndividualFeedbacks + System.Environment.NewLine + "</Individual Skills Feedback>" + System.Environment.NewLine;
+            stringMistakes = stringMistakes + System.Environment.NewLine + "</Mistakes>" + System.Environment.NewLine;
+        }
+
+        public void closeMainMenu()
+        {
+            MainCanvas.Children.Remove(mainMenu);
+        }
+        
+        
+
+        #endregion
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (this.frameReader != null)
+            {
+                this.frameReader.FrameArrived += frameReader_FrameArrived;
+            }
+        }
+     
+
+        void frameReader_FrameArrived(object sender, InfraredFrameArrivedEventArgs e)
+        {
+          //  rulesAnalyzer.AnalyseRules();
+            switch(myState)
+            {
+                case States.freestyle:
+                    rulesAnalyzerFIFO.AnalyseRules();
+                    freestyleMode.checkPause();
+                    break;
+                case States.individual:
+                    if(individualSkills.ready)
+                    {
+                        individualSkills.analyze();
+                    }
+                    break;
+            }
+            
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+          //  audioHandlerControl.close();
+           // bodyFrameHandlerControl.close();
+            videoHandler.close();
+
+        }
+    }
+}
